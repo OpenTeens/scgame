261297cae17f91fb634415340c43d71dd6778e58
Morax <James20081204@gmail.com>
2024-07-20 16:09:14 +0800

    不知道对不对。。

--- scgame/mixins/motion/__init__.py
+++ scgame/mixins/motion/__init__.py
@@ -1,18 +1,61 @@
 import math
-
 from scgame.sprite.base_sprite import BaseSprite
 from scgame.utils import deg2rad
 
 
 class MotionMixin(BaseSprite):
-    def move_forward(self, distance):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.speed = 0  # 默认速度设置为0
+
+    def move_forward(self, distance=None):
         """
-        Move forward.
-        :param distance: distance to move
+        向前移动
+        :param distance: 要移动的距离。如果为None，则使用当前速度。
         :return: None
         """
+        if distance is None:
+            distance = self.speed
         delta_x = distance * math.sin(deg2rad(self.direction))
         delta_y = distance * math.cos(deg2rad(self.direction))
+        self.x += delta_x
+        self.y += delta_y
+        self.game.refresh()
+
+    def rotate(self, angle):
+        """
+        旋转精灵
+        :param angle: 要旋转的角度（度）。
+        :return: None
+        """
+        self.direction = (self.direction + angle) % 360
+        self.game.refresh()
+
+    def set_speed(self, speed):
+        """
+        设置精灵的速度
+        :param speed: 精灵的新速度
+        :return: None
+        """
+        self.speed = speed
+
+    def stop(self):
+        """
+        停止精灵移动，将速度设置为0
+        :return: None
+        """
+        self.speed = 0
+
+    def move_backward(self, distance=None):
+        """
+        向后移动
+        :param distance: 要移动的距离。如果为None，则使用当前速度。
+        :return: None
+        """
+        if distance is None:
+            distance = self.speed
+        delta_x = -distance * math.sin(deg2rad(self.direction))
+        delta_y = -distance * math.cos(deg2rad(self.direction))
         self.x += delta_x
         self.y += delta_y
         self.game.refresh()

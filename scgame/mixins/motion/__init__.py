bafeb79098b51c970d84364d0eb092b123a970b7
cxzlw <wsc_zds@qq.com>
2024-07-20 15:38:39 +0800

    some upd

--- scgame/mixins/motion/__init__.py
+++ scgame/mixins/motion/__init__.py
@@ -1,5 +1,18 @@
+import math
+
 from scgame.sprite.base_sprite import BaseSprite
+from scgame.utils import deg2rad
 
 
 class _Motion(BaseSprite):
-    pass
+    def move_forward(self, distance):
+        """
+        Move forward.
+        :param distance: distance to move
+        :return: None
+        """
+        delta_x = distance * math.sin(deg2rad(self.direction))
+        delta_y = distance * math.cos(deg2rad(self.direction))
+        self.x += delta_x
+        self.y += delta_y
+        self.game.refresh()
--- scgame/sprite/base_sprite.py
+++ scgame/sprite/base_sprite.py
@@ -31,14 +31,3 @@ class BaseSprite:
         self.global_lists = game.global_lists
         # TODO: Register all the listeners here
 
-    def move_forward(self, distance):
-        """
-        Move forward.
-        :param distance: distance to move
-        :return: None
-        """
-        delta_x = distance * math.sin(deg2rad(self.direction))
-        delta_y = distance * math.cos(deg2rad(self.direction))
-        self.x += delta_x
-        self.y += delta_y
-        self.game.refresh()
